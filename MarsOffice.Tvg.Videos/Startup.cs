00273c3f97a4f9e22bf3b72a785f3d8e9977a623
Alin Florin <alin@qoffice.ro>
2022-02-06 21:07:02 +0200

    f

---
+++ MarsOffice.Tvg.Videos/Startup.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(MarsOffice.Tvg.Videos.Startup))]
+namespace MarsOffice.Tvg.Videos
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            FunctionsHostBuilderContext context = builder.GetContext();
+            var env = Environment.GetEnvironmentVariable("AZURE_FUNCTIONS_ENVIRONMENT") ?? "Development";
+            builder.ConfigurationBuilder
+                .AddJsonFile(Path.Combine(context.ApplicationRootPath, "appsettings.json"), optional: true, reloadOnChange: false)
+                .AddJsonFile(Path.Combine(context.ApplicationRootPath, $"appsettings.{env}.json"), optional: true, reloadOnChange: false)
+                .AddEnvironmentVariables();
+        }
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            
+        }
+    }
+}
