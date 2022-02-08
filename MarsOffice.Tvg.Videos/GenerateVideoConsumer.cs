50c8e483c5f6d0bcdf80a3747c03cd5d4e6c9aea
Alin Florin <alin@qoffice.ro>
2022-02-08 02:39:07 +0200

    -

--- MarsOffice.Tvg.Videos/GenerateVideoConsumer.cs
+++ MarsOffice.Tvg.Videos/GenerateVideoConsumer.cs
@@ -1,11 +1,15 @@
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
 using MarsOffice.Tvg.Videos.Abstractions;
 using MarsOffice.Tvg.Videos.Entities;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.Azure.Cosmos.Table;
+using Microsoft.Azure.SignalR.Management;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace MarsOffice.Tvg.Videos
@@ -13,9 +17,12 @@ namespace MarsOffice.Tvg.Videos
     public class GenerateVideoConsumer
     {
         private readonly IMapper _mapper;
-        public GenerateVideoConsumer(IMapper mapper)
+        private readonly IConfiguration _config;
+
+        public GenerateVideoConsumer(IMapper mapper, IConfiguration config)
         {
             _mapper = mapper;
+            _config = config;
         }
 
         [FunctionName("GenerateVideoConsumer")]
@@ -24,7 +31,8 @@ namespace MarsOffice.Tvg.Videos
         [Queue("start-processing", Connection = "localsaconnectionstring")] IAsyncCollector<StartProcessing> startProcessingQueue,
         ILogger log)
         {
-            var newVideo = new VideoEntity { 
+            var newVideo = new VideoEntity
+            {
                 Id = Guid.NewGuid().ToString(),
                 JobId = request.Job.Id,
                 ETag = "*",
@@ -39,10 +47,28 @@ namespace MarsOffice.Tvg.Videos
 
             var insertOperation = TableOperation.InsertOrReplace(newVideo);
             await videosTable.ExecuteAsync(insertOperation);
+            var dto = _mapper.Map<Video>(newVideo);
+            
+            try
+            {
+                using var serviceManager = new ServiceManagerBuilder()
+                    .WithOptions(option =>
+                    {
+                        option.ConnectionString = _config["signalrconnectionstring"];
+                    })
+                    .BuildServiceManager();
+                using var hubContext = await serviceManager.CreateHubContextAsync("main", CancellationToken.None);
+                await hubContext.Clients.User(request.Job.UserId).SendAsync("videoUpdate", dto, CancellationToken.None);
+            }
+            catch (Exception e)
+            {
+                log.LogError(e, "SignalR sending error");
+            }
 
-            await startProcessingQueue.AddAsync(new StartProcessing {
+            await startProcessingQueue.AddAsync(new StartProcessing
+            {
                 Job = request.Job,
-                Video = _mapper.Map<Video>(newVideo)
+                Video = dto
             });
             await startProcessingQueue.FlushAsync();
         }
--- MarsOffice.Tvg.Videos/StartProcessingConsumer.cs
+++ MarsOffice.Tvg.Videos/StartProcessingConsumer.cs
@@ -1,16 +1,30 @@
 using System;
+using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
 using MarsOffice.Tvg.Videos.Abstractions;
 using MarsOffice.Tvg.Videos.Entities;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.Azure.Cosmos.Table;
+using Microsoft.Azure.SignalR.Management;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace MarsOffice.Tvg.Videos
 {
     public class StartProcessingConsumer
     {
+        private readonly IMapper _mapper;
+        private readonly IConfiguration _config;
+
+        public StartProcessingConsumer(IMapper mapper, IConfiguration configuration)
+        {
+            _mapper = mapper;
+            _config = configuration;
+        }
+
         [FunctionName("StartProcessingConsumer")]
         public async Task Run([QueueTrigger("start-processing", Connection = "localsaconnectionstring")] StartProcessing request,
             [Table("Videos", Connection = "localsaconnectionstring")] CloudTable videosTable,
@@ -23,6 +37,25 @@ namespace MarsOffice.Tvg.Videos
                 Status = VideoStatus.Generating
             });
             await videosTable.ExecuteAsync(mergeOperation);
+
+            var dto = request.Video;
+            dto.Status = VideoStatus.Generating;
+
+            try
+            {
+                using var serviceManager = new ServiceManagerBuilder()
+                    .WithOptions(option =>
+                    {
+                        option.ConnectionString = _config["signalrconnectionstring"];
+                    })
+                    .BuildServiceManager();
+                using var hubContext = await serviceManager.CreateHubContextAsync("main", CancellationToken.None);
+                await hubContext.Clients.User(request.Job.UserId).SendAsync("videoUpdate", dto, CancellationToken.None);
+            }
+            catch (Exception e)
+            {
+                log.LogError(e, "SignalR sending error");
+            }
 
             // TODO
         }
