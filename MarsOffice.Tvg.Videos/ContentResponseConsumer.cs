2d950a469b76882d7ce3314f605fb9bb0838fe39
Alin Florin <alin@qoffice.ro>
2022-02-09 14:07:40 +0200

    f

---
+++ MarsOffice.Tvg.Videos/ContentResponseConsumer.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MarsOffice.Tvg.Content.Abstractions;
+using Microsoft.Azure.Cosmos.Table;
+using Microsoft.Azure.SignalR.Management;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace MarsOffice.Tvg.Videos
+{
+    public class ContentResponseConsumer
+    {
+        private readonly IMapper _mapper;
+        private readonly IConfiguration _config;
+
+        public ContentResponseConsumer(IMapper mapper, IConfiguration config)
+        {
+            _mapper = mapper;
+            _config = config;
+        }
+
+        [FunctionName("ContentResponseConsumer")]
+        public async Task Run(
+            [QueueTrigger("content-response", Connection = "localsaconnectionstring")]ContentResponse response,
+            [Table("Videos", Connection = "localsaconnectionstring")] CloudTable videosTable,
+            ILogger log)
+        {
+            try {
+
+            } catch (Exception e) {
+                log.LogError(e, "Function threw an exception");
+
+                try
+                {
+                    using var serviceManager = new ServiceManagerBuilder()
+                        .WithOptions(option =>
+                        {
+                            option.ConnectionString = _config["signalrconnectionstring"];
+                        })
+                        .BuildServiceManager();
+                    using var hubContext = await serviceManager.CreateHubContextAsync("main", CancellationToken.None);
+                    // await hubContext.Clients.User(response.UserId).SendAsync("videoUpdate", dto, CancellationToken.None);
+                }
+                catch (Exception ex)
+                {
+                    log.LogError(ex, "SignalR sending error");
+                }
+            }
+        }
+    }
+}
